b1fa157ca2f1fc18b469e37264ad5e288abb1e56
Izabel Vidal <izabelvidal99@gmail.com>
2020-12-15 12:04:29 -0300

    inciando jwt

--- src/main/java/io/iza/springsecurityjwt/HelloResource.java
+++ src/main/java/io/iza/springsecurityjwt/HelloResource.java
@@ -1,17 +1,14 @@
 package io.iza.springsecurityjwt;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 
-@Controller
+@RestController
 public class HelloResource {
     
     @RequestMapping({"/hello"})
     public String hello() {
         return "hello world!";
     }
-    
 }
